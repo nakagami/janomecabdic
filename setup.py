468f9ea05492d2132d8518c6ef6eeb6ae2225d65
Hajime Nakagami <nakagami@gmail.com>
2020-10-22 08:42:42 +0900

    fix MANIFEST.in

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ classifiers = [
 
 setup(
     name="janomecabdic",
-    version="0.1.0",
+    version="0.1.1",
     url='https://github.com/nakagami/janomecabdic/',
     classifiers=classifiers,
     license="LGPL2,BSD",
